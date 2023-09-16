c6405ce29ff0bff82a85e5e0b31c5279cfdfbb04
HyeongJoong Kim <fomula91@gmail.com>
2023-09-16 19:22:27 +0900

    feat: add naver login

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const axios = require('axios');
 require('dotenv').config();
 const app = express();
 const port = 3000;
 
 const client_id = process.env.NAVER_CLIENT_ID;
 const client_secret = process.env.NAVER_CLIENT_SECRET;
-const state = Math.random().toString().substring(3);
-const redirectURI = encodeURI("http://localhost:3000")
+
+const redirectURI = encodeURI("http://localhost:3000/callback")
 var api_url = ""
 
 app.get('/', (req, res) => {
@@ -14,32 +15,22 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', function(req, res) {
+    const state = Math.random().toString(36).substring(2,15);
     api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirectURI}&state=${state}`;
-    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-    res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
+    res.redirect(api_url)
 })
 
-app.get('/callback', function(req, res) {
-    code = req.query.code;
-    state = req.query.state;
-    console.log(code, state, "code , states")
-    api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='+client_id+'&client_secret='+client_secret+'&redirect_uri='+redirectURI+'&code='+code+'&state='+state;
-    var request = require('request');
-    var options = {
-        uri : api_url,
-        headers : {
-            'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret
-        }
+app.get('/callback', async (req, res) => {
+    const { code, state } = req.query
+    try {
+        api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='+client_id+'&client_secret='+client_secret+'&redirect_uri='+redirectURI+'&code='+code+'&state='+state;
+        const response = await axios.get(api_url);
+        const token = response.data.access_token;
+        res.send('인증성공!')
+    } catch (error) {
+        console.error('네이버 로그인 에러', error);
+        res.status(500).send('네이버 로그인에 실패하였습니다.')
     }
-    request.get(options, function(error, response, body){
-        if(!error && response.statusCode == 200){
-            res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-            res.end(body);
-        } else {
-            res.status(response.statusCode).end();
-            console.log('error = '+ response.statusCode );
-        }
-    })
 })
 
 app.listen(port, () => {
