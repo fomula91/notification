eacac97023779cf48ccff424f52d9d48c73795e6
HyeongJoong Kim <fomula91@gmail.com>
2023-10-04 13:46:51 +0900

    feat: add auto login, auto writing

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ var api_url = ""
 
 const state = Math.random().toString(36).substring(2,15);
 api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirectURI}&state=${state}`;
-let my_code, my_state;
-( async () => {
+
+const playwright = async () => {
+    let my_code, my_state;
     const browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
@@ -34,25 +35,67 @@ let my_code, my_state;
     const params = new URLSearchParams(new URL(page.url()).search);
     const state_value = params.get('state');
     const code_value = params.get('code')
-    // console.log("state: ",state_value, "code_token: ",  code_value)
+    // console.log("state: ",state_value, "code_token: ",+  code_value)
     my_code = code_value;
     my_state = state_value;
-    console.log('my_code: ', my_code, "\n", "my_state: ", my_state);
+    // console.log('my_code: ', my_code, "\n", "my_state: ", my_state);
 
     browser.close();
+    return {code : my_code, state: my_state}
+};
+//playwright block
+const runNaver = () => {
+    return new Promise( async (resolve, reject) => {
+        try{
+            await playwright().then((res) => resolve(res));
+        } catch(e) {
+            console.log(e);
+            reject(e);
+        }
+    })
+};
+runNaver().then( async (res) => {
+    const naver_code = res.code;
+    const naver_state = res.state;
+    const api_url = 
+    'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='+client_id+'&client_secret='+client_secret+'&redirect_uri='+redirectURI+'&code='+naver_code+'&state='+naver_state;
+    const response = await axios.get(api_url);
+    const { access_token, refresh_token } = response.data;
+    return { "ac_token" :access_token, "rf_token": refresh_token}
 
-})();
-console.log('outter -> my_code: ', my_code, "\n", "outter -> my_state: ", my_state);
+})
+.then( async (res) => {
+    
+    const ac = res.ac_token;
+    const rf = res.rf_token;
+    const api_url = `https://openapi.naver.com/v1/cafe/${clubid}/menu/${menuid}/articles`;
+    const token = "Bearer "+ ac; 
+    const headers = {
+        'Authorization': token,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    };
 
+    const response = await axios.post(api_url,{
+        subject: subject,  
+        content: content   
+    },{
+        headers : headers
+    } )
+
+    console.log(response.data)
+});
+
+// console.log('outter -> my_code: ', my_code, "\n", "outter -> my_state: ", my_state);
+// console.log('mycode => ', my_code);
 // twitch api
 const test_uset_id = process.env.TEST_USER_ID
 let twitch_token;
 let flag = false;
-axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_SECRET_KEY}&grant_type=client_credentials`)
-.then((res) => {
-    twitch_token = res.data.access_token
-    flag = true
-})
+// axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_SECRET_KEY}&grant_type=client_credentials`)
+// .then((res) => {
+//     twitch_token = res.data.access_token
+//     flag = true
+// })
 
 // setInterval(() => {
 //     if(flag) {
@@ -79,11 +122,11 @@ axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLI
 
 app.get('/')
 
-app.get('/login', function(req, res) {
-    // const state = Math.random().toString(36).substring(2,15);
-    api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirectURI}&state=${state}`;
-    res.redirect(api_url)
-})
+// app.get('/login', function(req, res) {
+//     // const state = Math.random().toString(36).substring(2,15);
+//     api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirectURI}&state=${state}`;
+//     res.redirect(api_url)
+// })
 
 app.get('/callback', async (req, res) => {
     const { code, state } = req.query
@@ -111,6 +154,7 @@ app.get('/postpage', (req, res) => {
 })
 
 app.post('/post', async (req, res) => {
+    console.log(req.headers, "headers")
     const userToken = req.headers.authorization && req.headers.authorization.split(' ')[1];
     console.log(userToken, 'userToken')
     const { subject, content } = req.body;
