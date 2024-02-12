c57dc9759622fa33072704e054e0317c19639e32
unknown <fomula91@gmail.com>
2024-02-12 23:08:21 +0900

    update : lint prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 들여쓰기에 사용할 공백 수
+  tabWidth: 2,
+  // 인용 부호 사용 방식('single' 또는 'double')
+  singleQuote: true,
+  // 줄 끝에 세미콜론을 사용할지 여부
+  semi: true,
+  // 작은 객체 리터럴에서 키에 인용 부호를 사용할지 여부
+  quoteProps: 'as-needed',
+  // 쉼표 후에 공백을 사용할지 여부
+  trailingComma: 'none'
+};
