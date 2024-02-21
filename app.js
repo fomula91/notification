0f58c54870547af50425d878cab7ba08164c50a6
fomula91 <fomula91@gmail.com>
2024-02-21 23:53:56 +0900

    fix: app crash hot fix

--- app.js
+++ app.js
@@ -199,43 +199,41 @@ let chzzkLiveID;
 const chzzkKeyword = encodeURI('마레플로스');
 const getChzzkLive = () => {
   console.log('hello chzzk api!');
-  try {
-    axios
-      .get(
-        `https://api.chzzk.naver.com/service/v1/search/channels?keyword=${chzzkKeyword}&offset=0&size=13&withFirstChannelContent=true`
-      )
-      .then((res) => {
-        const result = res.data.content.data;
-        if (
-          result[0].content.live !== null &&
-          result[0].channel.openLive === true
-        ) {
-          const liveID = result[0].content.live.liveId;
-          if (chzzkLiveID !== liveID) {
-            // console.log('live alive!!');
-            console.log(result[0]);
-            chzzkLiveID = liveID;
-            subject = encodeURI(
-              '[방송ON][치지직] ' + result[0].content.live.liveTitle
-            );
-            content = encodeURI(htmlWithoutNewlines);
-            runNaver();
-            setTimeout(getChzzkLive, 10000);
-            sendMessageTG('[치지직]' + result[0].content.live.liveTitle);
-          } else {
-            setTimeout(getChzzkLive, 10000);
-            console.log('현재 치지직 아이디와 저장된 치지직 아이디가 같음');
-          }
-        }
-        else{
-            setTimeout(getChzzkLive, 10000);
-            console.log('치지직 방송 정보 없음.. 다시 탐색합니다.')
-        }
-      });
-  } catch (e) {
+  axios
+  .get(
+    `https://api.chzzk.naver.com/service/v1/search/channels?keyword=${chzzkKeyword}&offset=0&size=13&withFirstChannelContent=true`
+  )
+  .then((res) => {
+    const result = res.data.content.data;
+    if (
+      result[0].content.live !== null &&
+      result[0].channel.openLive === true
+    ) {
+      const liveID = result[0].content.live.liveId;
+      if (chzzkLiveID !== liveID) {
+        // console.log('live alive!!');
+        console.log(result[0]);
+        chzzkLiveID = liveID;
+        subject = encodeURI(
+          '[방송ON][치지직] ' + result[0].content.live.liveTitle
+        );
+        content = encodeURI(htmlWithoutNewlines);
+        runNaver();
+        setTimeout(getChzzkLive, 10000);
+        sendMessageTG('[치지직]' + result[0].content.live.liveTitle);
+      } else {
+        setTimeout(getChzzkLive, 10000);
+        console.log('현재 치지직 아이디와 저장된 치지직 아이디가 같음');
+      }
+    }
+    else{
+        setTimeout(getChzzkLive, 10000);
+        console.log('치지직 방송 정보 없음.. 다시 탐색합니다.')
+    }
+  }).catch( e => {
     console.log(e);
-    sendMessageTG('치지직 오류!! :: \n', e);
-  }
+    sendMessageTG('axios error :: \n',e)
+  });
 };
 getChzzkLive();
 
