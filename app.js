24a4554896f17bceaa9b54bbd28b79a797007de8
unknown <fomula91@gmail.com>
2024-03-23 23:07:50 +0900

    adjust app.js

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
-// const { google } = require("googleapis");
-
 const port = 3000;
 
 const htmlFilePath = path.join(__dirname, "src", "body.html");
@@ -25,19 +23,11 @@ const NclientSecret = process.env.NAVER_CLIENT_TEST_SECRET;
 const clubid = process.env.NAVER_CLUB_ID;
 const menuid = process.env.NAVER_CLUB_MENU_ID;
 
-// const youtubeAPI = process.env.YOUTUBE_API_KEY;
-const { OAuth2Client } = require("google-auth-library");
-/* eslint-disable node/no-unpublished-require */
-const crenentials = require("./credentials.json");
-
-const { clientid, clientsecret, redirecturis } = crenentials.web;
 // const testChannelID = process.env.MARE_YOUTUBE_ID;
 let subject = encodeURI("생방송알림");
 let content = encodeURI(process.env.MARE_URL);
 
 const oAuth2Client = new OAuth2Client(clientid, clientsecret, redirecturis);
-
-const SCOPE = ["https://www.googleapis.com/auth/youtube.readonly"];
 
 const sendMessageTG = async (data) => {
   const botToken = process.env.TELEGRAM_ID;
@@ -48,65 +38,6 @@ const sendMessageTG = async (data) => {
     text: data.toString(),
   });
 };
-
-// let videoid;
-
-// const getYoutube = () => {
-//   const youtube = google.youtube({
-//     version: "v3",
-//     auth: oAuth2Client,
-//   });
-//   try {
-//     youtube.search.list(
-//       {
-//         part: "id",
-//         channelId: testChannelID,
-//         eventType: "live",
-//         type: "video",
-//       },
-//       (err, res) => {
-//         if (err) {
-//           console.error("api 오류", err);
-//           return;
-//         }
-
-//         const { items } = res.data;
-
-//         if (items && items.length > 0) {
-//           console.log("실시간 스트리밍");
-//           console.log(items);
-//           console.log(items[0].id.videoId);
-//           videoid = items[0].id.videoId;
-
-//           try {
-//             youtube.videos.list(
-//               {
-//                 part: "snippet",
-//                 id: videoid,
-//               },
-//               (err, res) => {
-//                 if (err) {
-//                   console.error("API 오류", err);
-//                   return;
-//                 }
-//                 const videoInfo = res.data.items[0].snippet;
-//                 console.log("동영상 제목:", videoInfo.title);
-//                 console.log("동영상 설명:", videoInfo.description);
-//               }
-//             );
-//           } catch (e) {
-//             console.log(e);
-//           }
-//         } else console.log("스트리밍 없슴");
-//       }
-//     );
-//     console.log("다시 탐색합니다...");
-//     // setTimeout(getYoutube, 10000);
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-// getYoutube();
 
 const redirectURI = encodeURI("http://localhost:3000/callback");
 let naverapiurl = "";
