10f151f993942dcb3127c058086955b9e0ca8114
KimHyeongJoong <fomula91@gmail.com>
2024-07-16 19:04:26 +0900

    chore: Update ecosystem.config.js to include xvfb configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,21 @@
 module.exports = {
   apps: [
     {
+      name: "xvfb",
+      script: "bash",
+      args: "-c 'Xvfb :99 -screen 0 1920x1080x24'",
+      autorestart: false,
+      watch: false,
+      env: {
+        DISPLAY: ":99",
+      },
+    },
+    {
       name: "app",
       script: "xvfb-run",
       args: "-a node ./app.js",
       instances: 1,
       exec_mode: "cluster",
-      watch: true,
       node_args: "--max-old-space-size=4096",
       env: {
         NODE_ENV: "development",
@@ -14,6 +23,12 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
       },
+      wait_ready: true,
+      autorestart: true,
+      watch: true,
+      restart_delay: 5000,
+      post_stop: "pkill Xvfb",
+      pre_start: "Xvfb :99 -screen 0 1920x1080x24 &",
     },
   ],
 };
